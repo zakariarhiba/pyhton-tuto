cbfc5e890832826e4d7f7740292515cc2d417114
zakaria <zakariarhiba@gmail.com>
2020-06-16 18:24:17 +0200

    add object.py

---
+++ object.py
@@ -0,0 +1,25 @@
+#object houya fin n9dr n5zno bzzf d les informations 3la 7aja m7adada
+
+#object syntax
+
+student = {
+    'first_name' : 'Zakaria',
+    'second-name' : 'RHIBA',
+    'age' : 18
+}
+
+
+#bach na5do ghi value
+print(student['first_name'])
+
+#ila bghina nzid chi value jdida
+student['class']='2STE'
+
+print(student)
+
+#ila bghina objct 5or wlkn mn lwl
+student2 = student.copy()
+student2['phone'] = '212 66 33 22'
+print(student2)
+
+#n9dro ndiro 7ta list kat7tawi 3la object ka value
