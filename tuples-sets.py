769a2024f62b34975bdb950d90a17ad5e07a8093
zakaria <zakariarhiba@gmail.com>
2020-06-16 18:03:47 +0200

    add tuples and sets

---
+++ tuples-sets.py
@@ -0,0 +1,39 @@
+# tuple hiya b7al list wlkn mat9drch tbdl fiha 
+
+# syntax deal tuple
+fruits = ('Apples', 'Oranges', 'Grapes')
+
+# ila bghiti value
+print(fruits[1])
+
+# mat9drch dir hadi bach tbdl
+fruits[0] = 'Pears'
+
+#del kandiroha ila bghina nm7iw chi val b7al tuple
+del fruits2
+
+# len db 3arfha ach gha t3tik ch7al mn 9ima kayna
+print(len(fruits))
+
+
+# set hiya ma3adhach index o mamrtbach
+
+#  set syntax
+fruits_set = {'Apples', 'Oranges', 'Mango'}
+
+# wach kayna value wstha 
+print('Apples' in fruits_set)
+
+# ila bghiti tzid fih
+fruits_set.add('Grape')
+
+# ila bghiti tm7i chi value
+fruits_set.remove('Grape')
+
+# tm7i values
+fruits_set.clear()
+
+# tm7iha
+del fruits_set
+
+print(fruits_set)
