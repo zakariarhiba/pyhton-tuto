52eb1bba0ae054b19a5ae83417808c4abe39e045
zakaria <zakariarhiba@gmail.com>
2020-06-16 18:37:29 +0200

    add functions

---
+++ functions.py
@@ -0,0 +1,15 @@
+#function bkolo basata houya code kanktbho f bloc wlkn maki5dm 7ta n3ayto 3lih
+
+#function syntax
+def sayHello(name):
+    print(f'hello {name} ')
+
+#ne3ayto 3lih
+sayHello('zakaria')
+
+#ila bghinaha t3tina value 
+#kandiro return fl5r o kan3tiwha chno trod lina 
+def addition(x,y):
+    return x + y
+
+print(addition(6,2))
