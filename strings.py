a4e11851ce153c39ebf884c8b9b2919d4b9a24c5
zakaria <zakariarhiba@gmail.com>
2020-06-16 17:17:59 +0200

    add strings

--- strings.py
+++ strings.py
@@ -2,5 +2,64 @@
 name = 'zakaria'
 age = 18
 
+#bach nbdlo mn int l string kandiro str(smiya d varible)
+#example : age = str(age)
 
-print('My name is ' + str(age))
+'''print('My name is ' + name )
+   print('I am ' + age)            ''' 
+#drna hado ''' 3la 9bl nbdaw comment fih bzzf deal les lines o kansaliw bihom
+
+#bach ndiro print b le variables kanktbohom haka
+#print('My name is {name} and I am {age}'.format(name=name,age=age))
+
+#o n9dro ndiro 7ta had method wlkn ghi f les versions jdida 3++
+#print(f'My name is {name} and I am {age}')
+
+#hado chwiya deal tricks li t9dr dirhom b strings jrbhom
+
+#1
+s = 'hello world'
+print(s.capitalize())
+
+
+#2 tkbr l7orof uppercase
+print(s.upper())
+
+#3 tsghr l7orof
+print(s.lower())
+
+#4 Swap case
+print(s.swapcase())
+
+#5  ch7al fiha mn 7arf
+print(len(s))
+
+#6 katbdl lwla b taniya
+print(s.replace('world', 'everyone'))
+
+#7 t7sb
+sub = 'h'
+print(s.count(sub))
+
+#8 wach kaybda b hello
+print(s.startswith('hello'))
+
+#9 wach kaysali b 'd'
+print(s.endswith('d'))
+
+#10 trodhom liste
+print(s.split())
+
+#11 t9lb 3la position dealo
+print(s.find('r'))
+
+#12 wach fih 7orof o ar9am
+print(s.isalnum())
+
+#13 wach kamlin 7orof
+print(s.isalpha())
+
+#14 wach kamlin ar9am
+print(s.isnumeric())
+
+
--- varible.py
+++ varible.py
@@ -1,11 +1,15 @@
-x = 10  #int
-y = 2.5  #float
-name = "zakaria"   #string str
-bachelier = True   #bool
+#drna '#'  bach nbdaw comment
 
+#hado le types deal les variables :
+age = 10             #int
+x = 2.5              #float
+smiya = "zakaria"    #string str
+bachelier = True     #bool
+
+#n9dro njm3ohom kamlin f str wa7d b7al haka
 x,y,name,bachelier = (10,2.5,"zakaria",True)  #list of varibles
 
+#haka gha ndiro varible wa7d wlkn fih bzzf deal values
 list = [1,2,3,4]   #array
-
-
+#had print bach t affiche lina chno kanktbo wstha
 print(list)
