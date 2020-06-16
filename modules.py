68128d345921b6f1e1b7f03081d5fd53c8152495
zakaria <zakariarhiba@gmail.com>
2020-06-16 22:25:35 +0200

    add modules.py

---
+++ modules.py
@@ -0,0 +1,13 @@
+# modulo houya wa7d l package deal code 
+# T9dr t5dm bih f lcode dealk wlkn 5sk Te3ayt 3lih
+
+# mital kifach ne3ayto 3lih
+import datetime
+
+#today = datetime.date.today()
+#print(today)
+
+#n9dro n7aydo datetime 
+from datetime import date
+today = date.today()
+print(today)
